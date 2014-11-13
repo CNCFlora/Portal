f33cfe58bd94b8b0cbcc7b449fe367e3514c9f95
Diogo Souza <diogo@diogok.net>
2014-11-13 12:23:34 -0200

    slideshow usando api de imagens

--- handlers/home.py
+++ handlers/home.py
@@ -1,5 +1,7 @@
 import jinja2
 import json
+import requests
+import urllib
 from flask.views import MethodView
 from flask import Flask, render_template, request, flash
 from base import BaseHandler
@@ -7,6 +9,18 @@ from base import BaseHandler
 class HomeHandler(MethodView, BaseHandler):
 
     def get(self):
+          images=[]
+          json_images = requests.get("http://cncflora.jbrj.gov.br/datahub/cncflora/_design/images/_view/tags?key=%22rdc%22&reduce=false")
+          all_images = json.loads(json_images.text)
+          for image in all_images["rows"]:
+              img = {}
+              cat = image["value"]["folder"]
+              img["name"] = cat[cat.rfind(':',0,cat.rfind(':')) + 1:].replace(":"," ")
+              img["link"] = 'http://cncflora.jbrj.gov.br/plataforma2/book/pub.php?id='+img["name"]
+              img["img"]  = 'http://cncflora.jbrj.gov.br/datahub/cncflora/'+urllib.quote( image["id"].encode('utf8') )+'/'+urllib.quote( image["value"]["metadata"]["title"].encode('utf8') ) 
+              images.append(img)
+          print images
           return  render_template('home.html',
+          images=images,
           base_url=self.base_url,
           static_url= self.static_url)
