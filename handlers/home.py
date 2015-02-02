c6e805186844a0f43bc8e3daac401f0757186b1b
Diogo Souza <diogo@diogok.net>
2015-02-02 17:19:44 -0200

    temp. disable sldeshow

--- handlers/home.py
+++ handlers/home.py
@@ -9,22 +9,6 @@ from base import BaseHandler
 class HomeHandler(MethodView, BaseHandler):
 
     def get(self):
-          images=[]
-          json_images = requests.get("http://cncflora.jbrj.gov.br/datahub/cncflora/_design/images/_view/tags?key=%22rdc%22&reduce=false")
-          all_images = json.loads(json_images.text)
-          for image in all_images["rows"]:
-              img = {}
-              cat = image["value"]["folder"]
-              img["name"] = cat[cat.rfind(':',0,cat.rfind(':')) + 1:].replace(":"," ")
-              img["img"]  = 'http://cncflora.jbrj.gov.br/datahub/cncflora/'+urllib.quote( image["id"].encode('utf8') )+'/'+urllib.quote( image["value"]["metadata"]["title"].encode('utf8') )
-
-              #url = 'http://'+self.services+'/assessments/taxon/'+urllib.quote(img["name"])
-              #r = requests.get(url)
-              #j = json.loads(r.text);
-              #img["category"] = j["category"]
-
-              images.append(img)
           return  render_template('home.html',
-          images=images,
           base_url=self.base_url,
           static_url= self.static_url)
